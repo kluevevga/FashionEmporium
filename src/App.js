07c32d5aa33ba359229fa7bd409c8db76a238f88
Евгений Клюев <craftycoderpro@gmail.com>
2021-10-04 15:43:24 +0300

    initialized enviroment with create-react-app & first component - homepage

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './App.css';
+
+const App = () => (
+  <div>
+    <HomePage/>
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
+
+
+// serviceWorkerRegistration.unregister();// register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
