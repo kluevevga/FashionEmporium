8702b4c53f7a791ef0e76e62416d443d7ce224c5
Евгений Клюев <craftycoderpro@gmail.com>
2021-10-17 14:44:48 +0300

    collection preveiew / collection overview

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import MenuItem from '../menu-item/menu-item.component';
-import sections from './directory.data';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import './directory.styles.scss';
 
 
-class Directory extends Component {
-    state = {
-        sections: sections
-    }
+const Directory = ({ sections }) => (
+    <div className='directory-menu'>
+        {
+            sections.map(section => <MenuItem key={section.id} {...section} />)
+        }
+    </div>
+);
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(section => <MenuItem key={section.id} {...section} />)
-                }
-            </div>
-        )
-    }
-};
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,21 +4,12 @@ import './menu-item.styles.scss';
 
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-    <div
-        className={`menu-item ${size ? 'large' : ''}`}
-        onClick={() => history.push(`${match.url}${linkUrl}`)}
-    >
-
-        <div
-            className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }}>
-        </div>
-
+    <div className={`menu-item ${size ? 'large' : ''}`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
-
     </div >
 );
 
